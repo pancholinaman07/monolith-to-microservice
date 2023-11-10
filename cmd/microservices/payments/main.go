bccc0692b2768cabcdf1bc2774d702396609c8cf
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-11 04:32:45 +0530

    Created payments microservice outline

--- cmd/microservices/payments/main.go
+++ cmd/microservices/payments/main.go
@@ -1,1 +1,41 @@
-package payments
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("starting payments microservice")
+
+	defer log.Println("closing payments microservice")
+
+	ctx := cmd.Context()
+
+	paymentsInterface := createPaymentMicroservice()
+
+	if err := paymentsInterface.Run(ctx); err != nil {
+		panic(err)
+	}
+}
+
+func createPaymentMicroservice() amqp.paymentsInterface {
+	cmd.WaitForService(os.Getenv("SHOP_RABBITMQ_ADDR"))
+
+	paymentsService := payments_app.NewPaymentService(
+		payments_infra_orders.NewHTTPClient(os.Getenv("SHOP_ORDERS_SERVICE_ADDR")),
+	)
+
+	paymentsInterface, err := amqp.NewPaymentsInterface(
+		fmt.Sprintf("amqp://%s/", os.Getenv("SHOP_RABBITMQ_ADDR")),
+		os.Getenv("SHOP_RABBITMQ_TO_PAY_QUEUE"),
+		paymentsService,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return paymentsInterface
+}
