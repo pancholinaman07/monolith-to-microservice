1d11b5bf24b686d5c9fa1bce6b3b80a17ecc121c
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-11 04:20:22 +0530

    Created shop microservice outline

--- cmd/microservices/shop/main.go
+++ cmd/microservices/shop/main.go
@@ -1,1 +1,42 @@
-package shop
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("starting shop microservice")
+
+	ctx := cmd.Context()
+
+	r := createShopMicroservice()
+
+	server := &http.Server{Addr: os.Getenv("SHOP_PRODUCT_SERVICE_BIND_ADDR"), Handler: r}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+
+	}()
+
+	<-ctx.Done()
+	log.Println("Closing shop microservice")
+
+	if err := server.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func createShopMicroservice() *chi.Mux {
+	shopProductRepo := shop_infra_product.NewMemoryRepository()
+
+	r := cmd.CreateRouter()
+
+	shop_interfaces_public_http.AddRoutes(r, shopProductRepo)
+	shop_interfaces_private_http.AddRoutes(r, shopProductRepo)
+
+	return r
+}
