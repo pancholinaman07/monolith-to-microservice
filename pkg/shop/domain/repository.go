8a681add09f16e3ff0aab0b10356691dc890a01e
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-11 15:05:26 +0530

    shop microservice domain repository created with add functinality

--- pkg/shop/domain/repository.go
+++ pkg/shop/domain/repository.go
@@ -1,1 +1,10 @@
-package domain
+package products
+
+import "errors"
+
+var ErrNotFound = errors.New("product not found")
+
+type Repository interface {
+	Save(*Product) error
+	ByID(ID) (*Product, error)
+}
